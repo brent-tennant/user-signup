79d55fa92f30e54a72e5892bca892df42ecabf63
brent-tennant <brenttennant8@gmail.com>
2017-07-06 11:06:50 -0700

    Finished User-Signup

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # user-signup
+
+#Instructions
+
+If the user's form submission is not valid, you should reject it and re-render the form with some feedback to inform the user of what they did wrong. The following things should trigger an error:
+
+The user leaves any of the following fields empty: username, password, verify password.
+The user's username or password is not valid -- for example, it contains a space character or it consists of less than 3 characters or more than 20 characters (e.g., a username or password of "me" would be invalid).
+The user's password and password-confirmation do not match.
+The user provides an email, but it's not a valid email. Note: the email field may be left empty, but if there is content in it, then it must be validated. The criteria for a valid email address in this assignment are that it has a single @, a single ., contains no spaces, and is between 3 and 20 characters long.
+Each feedback message should be next to the field that it refers to.
+
+For the username and email fields, you should preserve what the user typed, so they don't have to retype it. With the password fields, you should clear them, for security reasons.
+
+If all the input is valid, then you should show the user a welcome page that uses the username input to display a welcome message of: "Welcome, [username]!"
+
+Use templates (one for the index/home page and one for the welcome page) to render the HTML for your web app.
+While we've covered how to specify different input types than just text (e.g., password and email), for this assignment do not use the email input type. Instead, just use text, which does not do any client-side validation. This will enable us to check that the server side validation is working by letting errors through the client side. You should, however, use type='password' for the password and password verification inputs, to hide the characters typed (this input type does not included any additional validation).
+Submit
+To turn in your assignment and get credit, follow the submission instructions.
+
+Bonus Missions
+Use a Regular Expression (also called a regex) to validate the input fields. See this article about Regular Expressions for more information.
+
+If you haven't already, refactor your templates so that you are using a base template for the boilerplate HTML that is needed for both the home page and the welcome page, and template extensions for the additional HTML your app uses.
--- main.py
+++ main.py
@@ -4,8 +4,72 @@ import os
 import jinja2
 
 app = Flask(__name__)
-
 app.config['DEBUG'] = True
 
+@app.route("/")
+def index():
+    return render_template('signup-form.html')
+
+
 @app.route("/", methods=['POST'])
-def sign_up():
+def validate_fields():
+    username = request.form["username"]
+    password = request.form["password"]
+    verify_password = request.form["verify_password"]
+    email = request.form["email"]
+
+    username_error = ''
+    password_error = ''
+    verify_password_error = ''
+    email_error = ''
+
+    #Username too short or too long
+    if len(username) < 3:
+        username = ''
+        username_error = 'Username must be more than 3 characters'
+    elif len(username) > 20:
+        username = ''
+        username_error = 'Username must be less than 20 characters'
+    else:
+        username = username
+
+    #Password too short or too long
+    if 20 < len(password) < 3:
+        password = ''
+        password_error = 'Password must contain more than 3 character but no more than 20'
+
+    #Passwords don't match
+    if password != verify_password:
+        password = ''
+        verify_password = ''
+        verify_password_error = 'Passwords do not match'
+
+    #Email not valid
+    if len(email) > 0:
+        if not(email.endswith('@') or email.startswith('@') or email.endswith('.') or email.startswith('.')) and email.count('@') == 1 and email.count('.') == 1:
+            email=email
+        else:
+            email = ''
+            email_error = 'Email must contain @/. but not start or end with those characters'
+    else:
+        email = ''
+
+    #Empty Fields
+    if username == "":
+        username_error = 'Username must be more than 3 characters but no more than 20'
+    if password == "":
+        password_error = 'Password must contain more than 3 character but no more than 20'
+    if verify_password == "":
+        verify_password_error = 'Passwords do not match'
+
+
+    #Redirect to Confirmation Page if conditions are met
+    if not username_error and not password_error and not verify_password_error and not email_error:
+        return render_template('confirmation.html', username = username)
+
+    #Returns to signup-form if any conditions are not met
+    else:
+        return render_template('signup-form.html', username_error=username_error, password_error=password_error, verify_password_error=verify_password_error, email_error=email_error,
+        username=username, password=password, verify_password=verify_password, email=email)
+
+app.run()
